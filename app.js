c471fa9c4a7d681d93f146e6fd5529db0210561e
KyuHyuk Lee <lee@kyuhyuk.kr>
2021-01-06 23:36:52 +0900

    Add code to run  in asar archive

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
 const { remote } = require ('electron');
+const path = require ('path');
+const { execFile } = require('child_process');
 
-console.log(remote.app.getAppPath())
+let asarBinary = path.join(remote.app.getAppPath(), "hello.exe");
+console.log("'app.asar' Location : " + remote.app.getAppPath())
+console.log("'hello.exe' Location : " + asarBinary)
+
+execFile(asarBinary, null, (error, stdout, stderr) => {
+  if (error) {
+    throw error;
+  }
+  console.log(stdout);
+});
